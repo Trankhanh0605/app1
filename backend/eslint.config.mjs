f1ed6265a0b374862c196d2ed8c6f42ec3c07f0c
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-07-18 15:06:56 +0700

    style: reformat eslint configuration for improved readability

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,6 +2,11 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: { ...globals.node } } },
+  { files: 
+    ["**/*.js"], 
+    languageOptions: { sourceType: "commonjs" } },
+  { files: 
+    ["**/*.{js,mjs,cjs}"], 
+    languageOptions: 
+    { globals: { ...globals.node } } },
 ]);
