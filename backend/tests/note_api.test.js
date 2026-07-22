98976cffd0315dc2c44c3f4ed051cdf13e2d08f9
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-07-22 10:16:14 +0700

    feat: refactor notes retrieval to use async/await and add tests for note creation and validation

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -36,7 +36,7 @@ test('notes are returned as json', async () => {
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
@@ -44,6 +44,42 @@ test('a specific note is within the returned notes', async () => {
 
   const contents = response.body.map(e => e.content)
   assert(contents.includes('HTML is easy'))
+})
+
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 after(async () => {
