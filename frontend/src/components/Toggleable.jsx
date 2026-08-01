3fa57e552c145bc680a3f81ec731f2879e5c2ee0
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-08-01 09:44:09 +0700

    feat: add Toggleable component for conditional rendering

---
+++ frontend/src/components/Toggleable.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+function Toggleable(props){
+  const [visible, setVisible]=useState(false); 
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  function toggleVisibility(visible){
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>log in</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  );
+}
+export default Toggleable; 
