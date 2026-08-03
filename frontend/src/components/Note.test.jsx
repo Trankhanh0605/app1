5897948ae4bd12922aa42cb93f81a956e962f5e6
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-08-03 15:56:39 +0700

    test: add user event library and implement button click handler test for Note component

--- frontend/src/components/Note.test.jsx
+++ frontend/src/components/Note.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
+
 test('render content', () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
@@ -8,4 +10,23 @@ test('render content', () => {
   render(<Note note={note} />)
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
