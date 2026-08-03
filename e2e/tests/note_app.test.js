d36309454f08d0f8fba5b1e344c86648ac0f6bab
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-08-03 16:57:48 +0700

    chore: update test scripts, replace example test with note app suite, and restrict e2e tests to Microsoft Edge

---
+++ e2e/tests/note_app.test.js
@@ -0,0 +1,8 @@
+const { test, expect } = require("@playwright/test")
+
+test('front page can be opened', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+  const locator = page.getByText('Notes')
+  await expect(locator).toBeVisible()
+  await expect(page.getByText('Tran Nguyen Khanh, learning web dev')).toBeVisible()
+})
