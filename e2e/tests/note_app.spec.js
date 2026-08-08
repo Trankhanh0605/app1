32e9ddc5ed692c2697ad00c5c46304d9a0e6c030
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-08-08 10:42:08 +0700

    test: add login test, update playwright config, and wrap login inputs in labels

--- e2e/tests/note_app.spec.js
+++ e2e/tests/note_app.spec.js
@@ -1,8 +1,22 @@
 const { test, expect } = require("@playwright/test")
+const { describe } = require("node:test")
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
-  const locator = page.getByText('Notes')
-  await expect(locator).toBeVisible()
-  await expect(page.getByText('Tran Nguyen Khanh, learning web dev')).toBeVisible()
+describe('Note app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    const locator = page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Tran Nguyen Khanh, learning web dev')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    await page.getByRole('button', { name: 'login' }).click()
+    const textboxes = await page.getByRole('textbox').all()
+    await textboxes[0].fill('Khanh')
+    await textboxes[1].fill('khanh060506')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('TranKhanh logged in')).toBeVisible()
+  })
 })
+
